df82c568e7d9db8a260be7b7e8fa560e117d8c6e
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-05-17 10:45:45 -0300

    agora le todos os xml

--- load_file.cpp
+++ load_file.cpp
@@ -1,16 +1,42 @@
 #include "pugixml.hpp"
-
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <dirent.h>
+#include <string.h>
+
+void listFile();
 
 int main()
 {
 //[code_load_file
+    listFile();
+
     pugi::xml_document doc;
 
-    pugi::xml_parse_result result = doc.load_file("xmlLattes/0455487141833418.xml");
+    pugi::xml_parse_result result;
+    
+    
+    DIR *pDIR;
+	struct dirent *entry;
+	if( pDIR=opendir("./xmlLattes") ){
+		while(entry = readdir(pDIR)){
+			if( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 ){
+				result = doc.load_file((std::string("xmlLattes/") + entry->d_name).c_str());
+				std::cout << doc.child("CURRICULO-VITAE").child("DADOS-GERAIS").attribute("NOME-COMPLETO").value() << "\n";
+			}
+		}
+		closedir(pDIR);
+	}
 
-    std::cout << "Load result: " << result.description() << ", nome do pesquisador: " << doc.child("CURRICULO-VITAE").child("DADOS-GERAIS").attribute("NOME-COMPLETO").value() << std::endl;
+    //std::cout << "Load result: " << result.description() << ", nome do pesquisador: " << doc.child("CURRICULO-VITAE").child("DADOS-GERAIS").attribute("NOME-COMPLETO").value() << std::endl;
+    return 0;
 //]
 }
 
+void listFile() {
+	
+}
 // vim:et
