87d5ed01f02b5b8ff4563b1fa5b0dd7a6642a24b
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-06-18 19:28:16 -0300

    shit

--- src/main.h
+++ src/main.h
@@ -7,8 +7,8 @@
 
 //#define MAIN_WB
 //#define MAIN_TEST
-//#define MAIN_REGISTER
+#define MAIN_REGISTER
 //#define MAIN_SEQFILE
 //#define MAIN_LINKEDLIST
-#define MAIN_LATTESDB
+//#define MAIN_LATTESDB
 
